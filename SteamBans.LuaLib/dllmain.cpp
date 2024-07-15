dbfb5533505c866314a0deaab39db0f912306b8d
ColdMeekly <20516479+ColdMeekly@users.noreply.github.com>
2024-07-15 23:52:06 +0100

    Implemented LuaBans

---
+++ SteamBans.LuaLib/dllmain.cpp
@@ -0,0 +1,487 @@
+#include "ISteamBans.hpp"
+#include "windows_include.hpp"
+#include "steam_include.hpp"
+
+#pragma comment(lib, "SteamBans.SDK.lib")
+#include <luajit/lua.hpp>
+
+#include <algorithm>
+#include <mutex>
+#include <cassert>
+
+extern "C" __declspec(dllimport) ISteamBans* CreateSteamBansInterface();
+using namespace std::placeholders;
+
+namespace luabans
+{
+    ISteamBans* luabans_checkisteambans(lua_State* L, const int argnum) {
+        ISteamBans** ppSteamBans = reinterpret_cast<ISteamBans**>(luaL_checkudata(L, argnum, "ISteamBans"));
+
+        if (ppSteamBans == nullptr)
+        {
+            luaL_argerror(L, argnum, "Invalid ISteamBans");
+            return nullptr;
+        }
+
+        return *ppSteamBans;
+    }
+
+    ISteamBans::AccessType luabans_checkaccesstype(lua_State* L, const int argnum) {
+        ISteamBans::AccessType access = ISteamBans::AccessType(luaL_checkinteger(L, argnum));
+
+        if (access <= ISteamBans::AccessType::MIN || access > ISteamBans::AccessType::MAX)
+            luaL_argerror(L, argnum, "Invalid AccessType");
+
+        return access;
+    }
+
+    std::uint64_t luabans_checksteamid64(lua_State* L, const int argnum) {
+        size_t len;
+        const char* str = luaL_checklstring(L, argnum, &len);
+        std::string steamid(str, len);
+
+        if (steamid.size() < 17)
+            luaL_argerror(L, argnum, "steamid64 expected length 17");
+
+        // Ensure 17 numerical digits
+        if (!std::all_of(steamid.begin(), steamid.begin() + 17, ::isdigit))
+            luaL_argerror(L, argnum, "steamid64 must only contain digits");
+
+        // Convert the string to a steamid
+        const std::uint64_t steamid64 = std::stoull(steamid);
+
+        return steamid64;
+    }
+
+    void luabans_checkarglen(lua_State* L, const int expectedargs)
+    {
+        const int args = lua_gettop(L);
+        if (args != expectedargs)
+            luaL_error(L, "Expected %d args but got %d", expectedargs, args);
+    }
+
+    // void luabans:SetGlobalAccess(steambans.AccessType.Default)
+    int luabans_SetGlobalAccess(lua_State* L) {
+        luabans_checkarglen(L, 2);
+
+        lua_pushvalue(L, 1);
+        ISteamBans* pSteamBans = luabans_checkisteambans(L, 1);
+        ISteamBans::AccessType access = luabans_checkaccesstype(L, 2);
+
+        pSteamBans->SetGlobalAccess(access);
+        return 1;
+    }
+
+    // luabans.AccessType steambans:GetGlobalAccess()
+    int luabans_GetGlobalAccess(lua_State* L) {
+        luabans_checkarglen(L, 1);
+
+        lua_pushvalue(L, 1);
+        ISteamBans* pSteamBans = luabans_checkisteambans(L, 1);
+
+        lua_pushinteger(L, (lua_Integer)pSteamBans->GetGlobalAccess());
+        return 1;
+    }
+
+    // void steambans:SetUserAccess()
+    int luabans_SetUserAccess(lua_State* L) {
+        luabans_checkarglen(L, 3);
+
+        lua_pushvalue(L, 1);
+        ISteamBans* pSteamBans = luabans_checkisteambans(L, 1);
+        const std::uint64_t steamid = luabans_checksteamid64(L, 2);
+        ISteamBans::AccessType access = luabans_checkaccesstype(L, 3);
+
+        pSteamBans->SetUserAccess(steamid, access);
+        return 1;
+    }
+
+    // luabans.AccessType steambans:GetUserAccess()
+    int luabans_GetUserAccess(lua_State* L) {
+        luabans_checkarglen(L, 2);
+
+        ISteamBans* pSteamBans = luabans_checkisteambans(L, 1);
+        const std::uint64_t steamid = luabans_checksteamid64(L, 2);
+        
+        lua_pushinteger(L, (lua_Integer)pSteamBans->GetUserAcccess(steamid));
+        return 1;
+    }
+
+    // void steambans:SetFriendsAccess()
+    int luabans_SetFriendsAccess(lua_State* L) {
+        luabans_checkarglen(L, 2);
+
+        lua_pushvalue(L, 1);
+        ISteamBans* pSteamBans = luabans_checkisteambans(L, 1);
+        ISteamBans::AccessType access = luabans_checkaccesstype(L, 2);
+
+        pSteamBans->SetFriendsAccess(access);
+        return 1;
+    }
+
+    // luabans.AccessType steambans:GetFriendsAccess()
+    int luabans_GetFriendsAccess(lua_State* L) {
+        luabans_checkarglen(L, 1);
+
+        ISteamBans* pSteamBans = luabans_checkisteambans(L, 1);
+
+        lua_pushinteger(L, (lua_Integer)pSteamBans->GetFriendsAccess());
+        return 1;
+    }
+
+    // void steambans:SetBlockedAccess()
+    int luabans_SetBlockedAccess(lua_State* L) {
+        luabans_checkarglen(L, 2);
+
+        lua_pushvalue(L, 1);
+        ISteamBans* pSteamBans = luabans_checkisteambans(L, 1);
+        ISteamBans::AccessType access = luabans_checkaccesstype(L, 2);
+
+        pSteamBans->SetBlockedAccess(access);
+        return 1;
+    }
+
+    // luabans.AccessType steambans:GetBlockedAccess()
+    int luabans_GetBlockedAccess(lua_State* L) {
+        luabans_checkarglen(L, 1);
+
+        ISteamBans* pSteamBans = luabans_checkisteambans(L, 1);
+
+        lua_pushinteger(L, (lua_Integer)pSteamBans->GetBlockedAccess());
+        return 1;
+    }
+
+    // table<steamid, accesstype> steambans:GetUserAccessList()
+    int luabans_GetUserAccessList(lua_State* L) {
+        luabans_checkarglen(L, 1);
+
+        ISteamBans* pSteamBans = luabans_checkisteambans(L, 1);
+
+        auto& accessList = pSteamBans->GetUserAccessList();
+
+        lua_createtable(L, 0, accessList.size());
+        for (const auto& [steamid, access] : accessList)
+        {
+            lua_pushstring(L, std::to_string(steamid).c_str());
+            lua_pushinteger(L, (lua_Integer)access);
+            lua_rawset(L, -3);
+        }
+        
+        return 1;
+    }
+
+    /*enum class _ESteamNetworkingConnectionState
+    {
+        k_ESteamNetworkingConnectionState_None = 0,
+        k_ESteamNetworkingConnectionState_Connecting = 1,
+        k_ESteamNetworkingConnectionState_FindingRoute = 2,
+        k_ESteamNetworkingConnectionState_Connected = 3,
+        k_ESteamNetworkingConnectionState_ClosedByPeer = 4,
+        k_ESteamNetworkingConnectionState_ProblemDetectedLocally = 5,
+        k_ESteamNetworkingConnectionState_FinWait = -1,
+        k_ESteamNetworkingConnectionState_Linger = -2,
+        k_ESteamNetworkingConnectionState_Dead = -3,
+        k_ESteamNetworkingConnectionState__Force32Bit = 0x7fffffff
+    };*/
+
+    const char* ConnState_ToString(const ConnState state)
+    {
+        switch (state)
+        {
+        case ConnState::k_ESteamNetworkingConnectionState_None:
+            return "None";
+        case ConnState::k_ESteamNetworkingConnectionState_Connecting:
+            return "Connecting";
+        case ConnState::k_ESteamNetworkingConnectionState_FindingRoute:
+            return "FindingRoute";
+        case ConnState::k_ESteamNetworkingConnectionState_Connected:
+            return "Connected";
+        case ConnState::k_ESteamNetworkingConnectionState_ClosedByPeer:
+            return "ClosedByPeer";
+        case ConnState::k_ESteamNetworkingConnectionState_ProblemDetectedLocally:
+            return "ProblemDetectedLocally";
+        case ConnState::k_ESteamNetworkingConnectionState_FinWait:
+            return "FinWait";
+        case ConnState::k_ESteamNetworkingConnectionState_Linger:
+            return "Linger";
+        case ConnState::k_ESteamNetworkingConnectionState_Dead:
+            return "Dead";
+        case ConnState::k_ESteamNetworkingConnectionState__Force32Bit:
+            return "_Force32Bit";
+        default:
+            return "Unknown";
+        }
+    }
+
+    ISteamBans::AccessType DoAskCallback(lua_State* L, const char* callback, const std::uint64_t steamid, const ConnState oldState, const ConnState newState)
+    {
+        lua_getfield(L, LUA_REGISTRYINDEX, "steambans_callbacks");
+        lua_getfield(L, -1, callback);
+
+        if (lua_type(L, -1) == LUA_TFUNCTION)
+        {
+            lua_pushstring(L, std::to_string(steamid).c_str());
+            lua_pushstring(L, ConnState_ToString(oldState));
+            lua_pushstring(L, ConnState_ToString(newState));
+            lua_call(L, 3, 1);
+            ISteamBans::AccessType access = luabans_checkaccesstype(L, -1);
+
+            lua_pop(L, 2);
+
+            return access;
+        }
+
+        lua_pop(L, 3);
+
+        return ISteamBans::AccessType::Allow;
+    }
+
+    // void steambans:SetGlobalAskCallback()
+    int luabans_SetGlobalAskCallback(lua_State* L) {
+        luabans_checkarglen(L, 2);
+
+        ISteamBans* pSteamBans = luabans_checkisteambans(L, 1);
+        luaL_checktype(L, 2, LUA_TFUNCTION);
+
+        lua_getfield(L, LUA_REGISTRYINDEX, "steambans_callbacks");
+        lua_pushvalue(L, 2);
+        lua_setfield(L, -2, "global");
+
+        pSteamBans->SetGlobalAskCallback(std::bind(DoAskCallback, L, "global", _1, _2, _3));
+
+        lua_pop(L, 1);
+        lua_pushnil(L);
+
+        return 1;
+    }
+
+    // void steambans:SetUserAskCallback()
+    int luabans_SetUserAskCallback(lua_State* L) {
+        luabans_checkarglen(L, 2);
+
+        ISteamBans* pSteamBans = luabans_checkisteambans(L, 1);
+        luaL_checktype(L, 2, LUA_TFUNCTION);
+
+        lua_getfield(L, LUA_REGISTRYINDEX, "steambans_callbacks");
+        lua_pushvalue(L, 2);
+        lua_setfield(L, -2, "user");
+
+        pSteamBans->SetGlobalAskCallback(std::bind(DoAskCallback, L, "user", _1, _2, _3));
+
+        lua_pop(L, 1);
+        lua_pushnil(L);
+
+        return 1;
+    }
+
+    // void steambans:SetFriendsAskCallback()
+    int luabans_SetFriendsAskCallback(lua_State* L) {
+        luabans_checkarglen(L, 2);
+
+        ISteamBans* pSteamBans = luabans_checkisteambans(L, 1);
+        luaL_checktype(L, 2, LUA_TFUNCTION);
+
+        lua_getfield(L, LUA_REGISTRYINDEX, "steambans_callbacks");
+        lua_pushvalue(L, 2);
+        lua_setfield(L, -2, "friends");
+
+        pSteamBans->SetGlobalAskCallback(std::bind(DoAskCallback, L, "friends", _1, _2, _3));
+
+        lua_pop(L, 1);
+        lua_pushnil(L);
+
+        return 1;
+    }
+
+    // void steambans:SetBlockedAskCallback()
+    int luabans_SetBlockedAskCallback(lua_State* L) {
+        luabans_checkarglen(L, 2);
+
+        ISteamBans* pSteamBans = luabans_checkisteambans(L, 1);
+        luaL_checktype(L, 2, LUA_TFUNCTION);
+
+        lua_getfield(L, LUA_REGISTRYINDEX, "steambans_callbacks");
+        lua_pushvalue(L, 2);
+        lua_setfield(L, -2, "blocked");
+
+        pSteamBans->SetGlobalAskCallback(std::bind(DoAskCallback, L, "blocked", _1, _2, _3));
+
+        lua_pop(L, 1);
+        lua_pushnil(L);
+
+        return 1;
+    }
+
+    // table<steamid, lightuserdata> steambans:GetConnections()
+    int luabans_GetConnected(lua_State* L) {
+        luabans_checkarglen(L, 1);
+
+        ISteamBans* pSteamBans = luabans_checkisteambans(L, 1);
+
+        auto& connections = pSteamBans->GetConnections();
+
+        lua_createtable(L, connections.size(), 0);
+        int idx = 1;
+        for (const auto& [steamid, connection] : connections)
+        {
+            lua_pushinteger(L, idx++);
+            lua_pushstring(L, std::to_string(steamid).c_str());
+            lua_rawset(L, -3);
+        }
+
+        return 1;
+    }
+
+    int luabans_CloseConnection(lua_State* L) {
+        luabans_checkarglen(L, 5);
+
+        ISteamBans* pSteamBans = luabans_checkisteambans(L, 1);
+        const std::uint64_t steamid = luabans_checksteamid64(L, 2);
+        const int reason = luaL_checkinteger(L, 3);
+        const char* msg = luaL_checklstring(L, 4, 0);
+        if (!lua_isboolean(L, 5))
+            luaL_argerror(L, 5, "boolean expected");
+        const bool linger = lua_toboolean(L, 5);
+
+        auto& connections = pSteamBans->GetConnections();
+        if (auto connection = connections.find(steamid); connection != connections.end())
+            SteamNetworkingSockets()->CloseConnection(connection->second, reason, msg, linger);
+
+        lua_pushnil(L);
+        return 1;
+    }
+
+    // bool steambans:Attach()
+    int luabans_Attach(lua_State* L) {
+        luabans_checkarglen(L, 1);
+
+        ISteamBans* pSteamBans = luabans_checkisteambans(L, 1);
+        lua_pushboolean(L, pSteamBans->Attach());
+        
+        lua_pushvalue(L, 1);
+        lua_setfield(L, LUA_REGISTRYINDEX, "luabans_interface");
+
+        return 1;
+    }
+
+    // bool steambans:Detach()
+    int luabans_Detach(lua_State* L) {
+        luabans_checkarglen(L, 1);
+
+        ISteamBans* pSteamBans = luabans_checkisteambans(L, 1);
+        lua_pushboolean(L, pSteamBans->Detach());
+
+        lua_pushnil(L);
+        lua_setfield(L, LUA_REGISTRYINDEX, "luabans_interface");
+
+        return 1;
+    }
+
+    int luabans_getInterface(lua_State* L) {
+        ISteamBans** ppSteamBans = (ISteamBans**)lua_newuserdata(L, 8);
+        *ppSteamBans = CreateSteamBansInterface();
+
+        luaL_newmetatable(L, "ISteamBans");
+        
+
+        lua_createtable(L, 0, 17);
+
+        lua_pushcclosure(L, luabans_SetGlobalAccess, 0);
+        lua_setfield(L, -2, "SetGlobalAccess");
+        lua_pushcclosure(L, luabans_GetGlobalAccess, 0);
+        lua_setfield(L, -2, "GetGlobalAccess");
+
+        lua_pushcclosure(L, luabans_SetUserAccess, 0);
+        lua_setfield(L, -2, "SetUserAccess");
+        lua_pushcclosure(L, luabans_GetUserAccess, 0);
+        lua_setfield(L, -2, "GetUserAccess");
+
+        lua_pushcclosure(L, luabans_SetFriendsAccess, 0);
+        lua_setfield(L, -2, "SetFriendsAccess");
+        lua_pushcclosure(L, luabans_GetFriendsAccess, 0);
+        lua_setfield(L, -2, "GetFriendsAccess");
+
+        lua_pushcclosure(L, luabans_SetBlockedAccess, 0);
+        lua_setfield(L, -2, "SetBlockedAccess");
+        lua_pushcclosure(L, luabans_GetBlockedAccess, 0);
+        lua_setfield(L, -2, "GetBlockedAccess");
+
+        lua_pushcclosure(L, luabans_GetUserAccessList, 0);
+        lua_setfield(L, -2, "GetUserAccessList");
+
+        lua_pushcclosure(L, luabans_SetGlobalAskCallback, 0);
+        lua_setfield(L, -2, "SetGlobalAskCallback");
+        lua_pushcclosure(L, luabans_SetUserAskCallback, 0);
+        lua_setfield(L, -2, "SetUserAskCallback");
+        lua_pushcclosure(L, luabans_SetFriendsAskCallback, 0);
+        lua_setfield(L, -2, "SetFriendsAskCallback");
+        lua_pushcclosure(L, luabans_SetBlockedAskCallback, 0);
+        lua_setfield(L, -2, "SetBlockedAskCallback");
+
+        lua_pushcclosure(L, luabans_GetConnected, 0);
+        lua_setfield(L, -2, "GetConnected");
+
+        lua_pushcclosure(L, luabans_CloseConnection, 0);
+        lua_setfield(L, -2, "CloseConnection");
+
+        lua_pushcclosure(L, luabans_Attach, 0);
+        lua_setfield(L, -2, "Attach");
+        lua_pushcclosure(L, luabans_Detach, 0);
+        lua_setfield(L, -2, "Detach");
+
+
+        lua_setfield(L, -2, "__index");
+
+        lua_pushcclosure(L, luabans_Detach, 0);
+        lua_setfield(L, -2, "__gc");
+
+        lua_setmetatable(L, -2);
+
+        return 1;
+    }
+};
+
+
+static const struct luaL_Reg functions[] = {
+    {"getInterface", luabans::luabans_getInterface},
+    {NULL, NULL}
+};
+
+
+extern "C" {
+    __declspec(dllexport) int luaopen_luabans(lua_State* L) {
+        luaL_register(L, "luabans", functions);
+
+        // Register AccessType enum
+        lua_createtable(L, 4, 4);
+
+        lua_pushinteger(L, (int)ISteamBans::AccessType::Default);
+        lua_setfield(L, -2, "Default");
+        lua_pushinteger(L, (int)ISteamBans::AccessType::Deny);
+        lua_setfield(L, -2, "Deny");
+        lua_pushinteger(L, (int)ISteamBans::AccessType::Allow);
+        lua_setfield(L, -2, "Allow");
+        lua_pushinteger(L, (int)ISteamBans::AccessType::Ask);
+        lua_setfield(L, -2, "Ask");
+
+        lua_pushinteger(L, (int)ISteamBans::AccessType::Default);
+        lua_pushliteral(L, "Default");
+        lua_rawset(L, -3);
+        lua_pushinteger(L, (int)ISteamBans::AccessType::Deny);
+        lua_pushliteral(L, "Deny");
+        lua_rawset(L, -3);
+        lua_pushinteger(L, (int)ISteamBans::AccessType::Allow);
+        lua_pushliteral(L, "Allow");
+        lua_rawset(L, -3);
+        lua_pushinteger(L, (int)ISteamBans::AccessType::Ask);
+        lua_pushliteral(L, "Ask");
+        lua_rawset(L, -3);
+
+        lua_setfield(L, -2, "AccessType");
+
+        lua_newtable(L);
+        lua_setfield(L, LUA_REGISTRYINDEX, "steambans_callbacks");
+
+        return 1;
+    }
+}
---
+++ SteamBans.LuaLib/luabans.lua
@@ -0,0 +1,33 @@
+---@class luabans
+---@field getInterface fun(): ISteamBans Retrieves the LuaBans interface.
+luabans = {}
+
+---@enum AccessType
+luabans.AccessType =
+{
+    MIN         = 0,
+    Default     = 1,
+    Deny        = 2,
+    Allow       = 3,
+    Ask         = 4,
+    MAX         = 5
+}
+
+---@class ISteamBans
+---@field SetGlobalAccess fun(self: ISteamBans, access: AccessType): nil Sets the global access rights.
+---@field GetGlobalAccess fun(self: ISteamBans): AccessType Retrieves the global access rights.
+---@field SetUserAccess fun(self: ISteamBans, steamid: string, access: AccessType): nil Sets the access rights for a specific user.
+---@field GetUserAccess fun(self: ISteamBans, steamid: string): AccessType Retrieves the access rights for a specific user.
+---@field SetFriendsAccess fun(self: ISteamBans, access: AccessType): nil Sets the access rights for all of user's friends.
+---@field GetFriendsAccess fun(self: ISteamBans): AccessType Retrieves the access rights for all of user's friends.
+---@field SetBlockedAccess fun(self: ISteamBans, access: AccessType): nil Sets the access rights for all of user's blocked users.
+---@field GetBlockedAccess fun(self: ISteamBans): AccessType Retrieves the access rights for all of user's blocked users.
+---@field GetUserAccessList fun(self: ISteamBans): table<string, AccessType> Retrieves the access rights for all users.
+---@field SetGlobalAskCallback fun(self: ISteamBans, callback: fun(steamid: string, old: string, new: string): AccessType): nil Sets the global access callback.
+---@field SetUserAskCallback fun(self: ISteamBans, steamid: string, callback: fun(steamid: string, old: string, new: string): AccessType): nil Sets the access callback for a specific user.
+---@field SetFriendsAskCallback fun(self: ISteamBans, callback: fun(steamid: string, old: string, new: string): AccessType): nil Sets the access callback for all of user's friends.
+---@field SetBlockedAskCallback fun(self: ISteamBans, callback: fun(steamid: string, old: string, new: string): AccessType): nil Sets the access callback for all of user's blocked users.
+---@field GetConnected fun(self: ISteamBans): table<string> Returns a list of all connected users steamids. Only accessible after attaching.
+---@field CloseConnection fun(self: ISteamBans, steamid: string, reason: integer, message: string, linger: boolean): nil Closes a connection to a user.
+---@field Attach fun(self: ISteamBans): nil Attaches the interface to Steam.
+---@field Detach fun(self: ISteamBans): nil Detaches the interface from Steam.
